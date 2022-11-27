66da3a0c3f6bae315b7d6e8c76ae49de5ccd1482
Rabby <62188588+Maggotik@users.noreply.github.com>
2022-11-27 17:44:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -802,8 +802,8 @@ def new_order_go(isRepeat):
 
 be.loadJinjaGlobals()
 be.navigationLoadPages()
+be.logoutUser()
 
 if __name__ == '__main__': 
-    be.logoutUser()
     be.init()
     globals.app.run(debug=True)
